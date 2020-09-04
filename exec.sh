045cd40587bd12cb6d047dcf7a763c8529603e84
zobaed11 <zobaedsakib@gmail.com>
2020-09-04 06:36:39 -0500

    ready

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 rm -r -f cloud
-rm -r -f S3BD_client_unmodified/data/abstracts
-rm -r -f S3BD_client_unmodified/data/tmp
+rm -r -f ClientEncryptedSearch/data/abstracts
+rm -r -f ClientEncryptedSearch/data/tmp
 
 mkdir cloud
 mkdir cloud/cloudserver
@@ -8,8 +8,8 @@ mkdir cloud/cloudserver/storage
 mkdir cloud/cloudserver/utilities
 mkdir cloud/cloudserver/watch
 mkdir cloud/cloudserver/utilities/clusters
-mkdir S3BD_client_unmodified/data/abstracts
-mkdir S3BD_client_unmodified/data/tmp
+mkdir ClientEncryptedSearch/data/abstracts
+mkdir ClientEncryptedSearch/data/tmp
 
-cp S3BD_client_unmodified/input/* S3BD_client_unmodified/data/tmp
+cp ClientEncryptedSearch/input/* ClientEncryptedSearch/data/tmp
 
